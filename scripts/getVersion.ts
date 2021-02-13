6fe914fa5fa98966fbe499d5432a77b9b57ec377
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2021-02-13 22:39:04 +0900

    GitHub Action Release

---
+++ scripts/getVersion.ts
@@ -0,0 +1,8 @@
+export const getVersion = (): number => {
+    const matches = process?.version?.match(/^v(\d+\.\d+)/);
+    if (matches && matches.length > 1) {
+        return parseFloat(matches[1]);
+    } else {
+        return 12;
+    }
+};
--- scripts/post-pack.ts
+++ scripts/post-pack.ts
@@ -1,14 +1,28 @@
 import fs from 'fs';
 import glob from 'glob';
+import { getVersion } from './getVersion';
 
 const cleanUp = () => {
+    const version = getVersion();
     glob('packages/!(*.exe|*.dmg)', (err, matches) => {
         if (err) {
             console.error(err);
         } else {
             if (matches.length > 0) {
                 matches.forEach(item => {
-                    fs.rmSync(item, { recursive: true });
+                    if (fs.existsSync(item)) {
+                        if (version >= 15) {
+                            fs.rmSync(item, { recursive: true });
+                        } else {
+                            if (fs.lstatSync(item).isDirectory()) {
+                                fs.rmdirSync(item, { recursive: true });
+                            } else if (fs.lstatSync(item).isFile()) {
+                                fs.unlinkSync(item);
+                            }
+                        }
+
+                        // console.info(`${index + 1}: ${item}`);
+                    }
 
                     // console.info(`${index + 1}: ${item}`);
                 });
