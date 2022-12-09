2d21134d5d177cad24e32ec26bf23b99f89aac56
Pon Cheol Ku (구본철) <bbon@live.com>
2022-12-09 13:04:54 +0900

    feat: Update dependencies #118
    - Fix renderer duplicate rendering

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { RenameApp } from './components/RenameApp';
 import { SnackbarProvider } from 'notistack';
 import { ThemeProvider } from '@mui/system';
@@ -15,17 +14,15 @@ if (container) {
     const root = createRoot(container);
 
     root.render(
-        <React.StrictMode>
-            <React.Fragment>
-                <CssBaseline enableColorScheme />
-
-                <ThemeProvider theme={theme}>
-                    <SnackbarProvider maxSnack={4}>
-                        <RenameApp />
-                    </SnackbarProvider>
-                </ThemeProvider>
-            </React.Fragment>
-        </React.StrictMode>,
+        // <React.StrictMode></React.StrictMode>
+        <React.Fragment>
+            <CssBaseline enableColorScheme />
+            <ThemeProvider theme={theme}>
+                <SnackbarProvider maxSnack={4}>
+                    <RenameApp />
+                </SnackbarProvider>
+            </ThemeProvider>
+        </React.Fragment>,
     );
 } else {
     const rootElement = document.createElement('div');
@@ -35,13 +32,12 @@ if (container) {
     const root = createRoot(rootElement);
 
     root.render(
-        <React.StrictMode>
-            <React.Fragment>
-                <div>
-                    <h1>The app failed to start.</h1>
-                </div>
-            </React.Fragment>
-        </React.StrictMode>,
+        // <React.StrictMode></React.StrictMode>
+        <React.Fragment>
+            <div>
+                <h1>The app failed to start.</h1>
+            </div>
+        </React.Fragment>,
     );
 }
 // ReactDOM.render(
